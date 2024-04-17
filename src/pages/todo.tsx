45874838873a116364ef08ce4ca6ff2e8fc11f28
Ankur Agarwal <signup.ankur@gmail.com>
2024-04-17 14:57:08 +0530

    integrated provider

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,41 +1,7 @@
 import React, { useState } from "react";
 import { MdAdd, MdDelete } from "react-icons/md";
 import { FiSettings } from "react-icons/fi";
-import {
-  useCopilot,
-  CopilotConfigType,
-  CopilotProvider,
-  VoiceToSkillComponent,
-} from "@sugar-ai/copilot-one-js";
-
-const copilotConfig: CopilotConfigType = {
-  copilotId: "da82abb5-cf74-448b-b94d-7e17245cc5d9",
-  style: {
-    container: {
-      position: "bottom-right",
-    },
-  },
-
-  server: {
-    endpoint: "http://localhost:3000/api",
-    // endpoint: "https://staging.sugarcaneai.dev/api",
-    token: "pk-ZSl1A2XLH8AGY5JqkLDDX75BwZmKR7wCYL22Rcr8vVURvYVZ",
-
-    // headers: {
-    //   // optional headers, to be sent with each api request
-    //   'X-COPILOT-ID': '1234',
-    // },
-  },
-
-  ai: {
-    defaultPromptTemmplate: "pulkit.ag02/recipiess/recipie/0.0.1",
-    defaultPromptVariables: {
-      "#AGENT_NAME": "Tudy",
-    },
-    successResponse: "Task Done",
-    failureResponse: "I am not able to do this",
-  },
-};
+import { useCopilot } from "@sugar-ai/copilot-one-js";
 
 enum recurringType {
   none,
@@ -54,5 +20,5 @@ export default function Todo() {
   const [input, setInput] = useState("");
   const [filter, setFilter] = useState("all");
 
-  return <div>Todo</div>;
+  return <div>Todo yey</div>;
 }
