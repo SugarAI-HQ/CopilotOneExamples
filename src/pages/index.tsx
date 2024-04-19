bf1b06fc76c97629108f1e61f86f2209f9fc4d7a
Pulkit Agarwal <pulkit.ag02@gmail.com>
2024-04-19 16:55:49 +0530

    name change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">
-            SugarAI HQ
+            SugarAI
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Copilot Examples
